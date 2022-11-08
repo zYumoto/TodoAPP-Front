c44a382898c9e7586f1194f1bb74a6c673ac46e2
Victor <victorjuanrazer@gmail.com>
2022-11-08 15:48:37 -0300

    Add:Grid/IconButton

--- src/todo/TodoForm.jsx
+++ src/todo/TodoForm.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import React from 'react'
+import Grid from '../template/grid'
+import IconButton  from '../template/IconButton'
 
-export default props => (
 
-    <div>
-        <h1>
-        form
-        </h1>
-        </div>
+const TodoForm = props => (
+
+    <div role='form' className='todoForm'>
+        <Grid cols='12 9 10'>
+            <input id='description' className='form-control' placeholder='Adicione uma tarefa'></input>
+        </Grid>
+
+            <Grid cols='12 3 2'>
+                <IconButton style='primary' icon='plus'></IconButton>
+        </Grid>
+    </div>
 )
