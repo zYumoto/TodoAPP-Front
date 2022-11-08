2f009545792ccd101d2ecac4951c26e8f2e8572a
Victor <victorjuanrazer@gmail.com>
2022-11-08 15:40:22 -0300

    update:about

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import PageHeader from '../template/pageHeader'
 
-export default props => (
+const About = props => (
     <div>
-        <h1>sobre</h1>
-    </div>
+        <PageHeader name="Sobre" small="Nós"/>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        <h2>Missão e Valores</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>    </div>
 )
+export default About
---
+++ src/todo/Todo.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class Todo extends Component {
+    render(){
+        return(
+            <div>
+                <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+            </div>
+        )
+    }
+}
