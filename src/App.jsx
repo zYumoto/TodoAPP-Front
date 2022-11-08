190a677e51ef69bc794ae4df9073e03ff0a810c3
Victor <victorjuanrazer@gmail.com>
2022-11-08 15:13:34 -0300

    add: todo/about

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+require('bootstrap/dist/css/bootstrap.min.css')
+require('font-awesome/css/font-awesome.min.css')
+import React from "react"
+import Todo from '../todo/todo'
+import About from '../about/about'
+
+function App() {
+    <div className="container">
+    <Todo/>
+    <About/>
+</div>
+}
+
+export default App
---
+++ src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class Todo extends Component {
+    render(){
+        return(
+            <div>
+                <h1>TODO</h1>
+            </div>
+        )
+    }
+}
